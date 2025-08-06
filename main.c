8fe9e73c3e8aed28ba2f71e46ec4e3bfa45ec649
rafapiasentin <119822431+rafapiasentin@users.noreply.github.com>
2025-08-06 20:16:25 -0300

    06/08

---
+++ main.c
@@ -0,0 +1,16 @@
+//bibliuoteca que irá manipular a entrada e saida de dados 
+#include <stdio.h>
+#include <stdlib.h>
+
+//método main para execução
+int main () {
+    //codigo a ser desenvolvido
+
+    // \n comando para pular linha no print
+    printf ("hello, world!\n");
+
+    system("pause");
+
+    //retorno da minha função
+    return 0;
+}
