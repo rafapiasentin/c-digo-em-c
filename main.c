3d6bf7281bb095afd0b19fcbbb60e2b77dc112f4
rafapiasentin <119822431+rafapiasentin@users.noreply.github.com>
2025-08-06 21:35:09 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -6,8 +6,18 @@
 int main () {
     //codigo a ser desenvolvido
 
+    //TIPO DE VARIAVEIS
+    //caractere(char)  -127 a 127 letras (%s string= caractere infinito) (%c uma letra)
+    //inteiro(int) -32.767 a 32.767
+    //ponto flutuante(float) seis digitos de precisão
+    //ponto flutuante de precisão dupla(double) dez digitos de precisão
+    //sem valor(void) 0
+
     // \n comando para pular linha no print
     printf ("hello, world!\n");
+
+    char rafael[] = "rafael";
+    printf("nome: %s\n", rafael);
 
     system("pause");
 
